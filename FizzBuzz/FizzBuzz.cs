3c056886a4c3890effc5bd109350f88fec37d559
KWojna02 <136491557+KWojna02@users.noreply.github.com>
2024-03-03 19:47:46 +0100

    Removed some unnecessary curly braces

--- FizzBuzz/FizzBuzz.cs
+++ FizzBuzz/FizzBuzz.cs
@@ -11,32 +11,18 @@ namespace FizzBuzz
         private int upper;
 
         public FizzBuzz(int upper)
-        {
-            this.upper = upper;
+        { 
+            this.upper = upper; 
         }
 
         public void Display()
         {
             for (int i = 1; i < upper; i++)
             {
-                if (i % 3 == 0 && i % 5 == 0)
-                {
-                    Console.WriteLine("FizzBuzz");
-                }
-
-                else if (i % 3 == 0)
-                {
-                    Console.WriteLine("Fizz");
-                }
-                else if (i % 5 == 0)
-                {
-                    Console.WriteLine("Buzz");
-                }
-
-                else
-                {
-                    Console.WriteLine(i);
-                }
+                if (i % 3 == 0 && i % 5 == 0) Console.WriteLine("FizzBuzz");
+                else if (i % 3 == 0) Console.WriteLine("Fizz");
+                else if (i % 5 == 0) Console.WriteLine("Buzz");
+                else Console.WriteLine(i);
             }
         }
     }
