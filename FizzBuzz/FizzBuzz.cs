87024391726d1874e06c2fe5a14236617407da2b
KWojna02 <136491557+KWojna02@users.noreply.github.com>
2024-03-03 19:36:10 +0100

    Dodaj pliki projektów.

---
+++ FizzBuzz/FizzBuzz.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FizzBuzz
+{
+    internal class FizzBuzz
+    {
+        private int upper;
+
+        public FizzBuzz(int upper)
+        {
+            this.upper = upper;
+        }
+
+        public void Display()
+        {
+            for (int i = 1; i < upper; i++)
+            {
+                if (i % 3 == 0 && i % 5 == 0)
+                {
+                    Console.WriteLine("FizzBuzz");
+                }
+
+                else if (i % 3 == 0)
+                {
+                    Console.WriteLine("Fizz");
+                }
+                else if (i % 5 == 0)
+                {
+                    Console.WriteLine("Buzz");
+                }
+
+                else
+                {
+                    Console.WriteLine(i);
+                }
+            }
+        }
+    }
+
+}
+
+
---
+++ FizzBuzz/Program.cs
@@ -0,0 +1,13 @@
+﻿namespace FizzBuzz
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Wpisz gorna granice.");
+            int limit = int.Parse(Console.ReadLine());
+            FizzBuzz fz1 = new FizzBuzz(limit);
+            fz1.Display();
+        }
+    }
+}
